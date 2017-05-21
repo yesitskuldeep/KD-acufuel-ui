2c476c7562af59c64cec40772039291f728aa32f
anchit <anchit.jindal@viithiisys.com>
2017-05-21 16:22:47 +0530

    added new code

--- app/partials/ContactView/ContactView.controller.js
+++ app/partials/ContactView/ContactView.controller.js
@@ -3,13 +3,45 @@
  //Load controller
   angular.module('acufuel')
 
-	.controller('ContactViewController', ['$scope',function($scope) {
-
-      $scope.test = "Testing...";
-
-      $(function() {
-      $('#toggle-one12').bootstrapToggle();
-      $('#toggle-two21').bootstrapToggle();
+	.controller('ContactViewController', ['$scope', '$uibModal', 'ContactViewService', 'ViewCompanyService', function($scope, $uibModal, ContactViewService, ViewCompanyService) {
+      	$(function() {
+      		$('#toggle-one12').bootstrapToggle();
+      		$('#toggle-two21').bootstrapToggle();
         }) 
+        
+        $(document).ready(function() {
+            $('#contacts').DataTable();
+        });
+        
+        ContactViewService.getContacts().then(function(result) {
+        	console.log('==========',result);
+        	$scope.contactList = result;
+          
+        })
+        
+        ContactViewService.getCompanies().then(function(result) {
+        	console.log('==========',result);
+        	$scope.companies = result;
+          
+        })
+        
+        $scope.contactData = {};
+        $scope.contactData.contactList = [];
+        $scope.addContact = function(){
+          console.log('==== $scope.data======', $scope.data);
+          $scope.contactData.contactList.push($scope.data);
+          ViewCompanyService.addContact($scope.contactData).then(function(result) {
+            if(result.success){
+              toastr.success(''+result.success+'', {
+                  closeButton: true
+                })
+                $('#contact-modal-3').modal('hide');
+            }else{
+              toastr.error(''+result.statusText+'', {
+                  closeButton: true
+                })
+            }
+          })
+        }
 
     }]);
--- app/partials/ContactView/ContactView.service.js
+++ app/partials/ContactView/ContactView.service.js
@@ -1,13 +1,42 @@
 (function(){
  'use strict';    
     angular.module('acufuel')
-      .service('ContactViewService', ['$q', '$http', 'BE', ContactViewService]);
+      .service('ContactViewService', ['$q', '$http', 'BASE_URL', ContactViewService]);
 
-      function ContactViewService($q, $http, BE) {        
+      function ContactViewService($q, $http, BASE_URL) {        
         var temp = {};
 
+        this.getContacts = function() {
+
+            var deferred = $q.defer();
+            $http({
+                method : 'GET',
+                url : BASE_URL.url +'/user/allContacts',
+                headers : {'Content-Type': 'application/json'},
+            })
+            .then(function (result){
+                deferred.resolve(result.data);
+            },function (result){
+                deferred.resolve(result.data);
+            })
+            return deferred.promise;
+          }
         
-        
+        this.getCompanies = function() {
+
+            var deferred = $q.defer();
+            $http({
+                method : 'GET',
+                url : BASE_URL.url +'/user/allCompanies',
+                headers : {'Content-Type': 'application/json'},
+            })
+            .then(function (result){
+                deferred.resolve(result.data);
+            },function (result){
+                deferred.resolve(result.data);
+            })
+            return deferred.promise;
+          }
       }
       
 })();
