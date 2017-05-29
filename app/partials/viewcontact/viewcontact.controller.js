67c7d62d396c819b1308d1212ded935c8cd407bf
swarn <swarn.singh@viithiisys.com>
2017-05-29 14:25:53 +0530

    view contact page updated

--- app/partials/viewcontact/viewcontact.controller.js
+++ app/partials/viewcontact/viewcontact.controller.js
@@ -4,12 +4,14 @@
   angular.module('acufuel')
 
 	.controller('viewcontactController', ['$scope', '$stateParams', 'ViewcontactService', 'ViewCompanyService', function($scope, $stateParams, ViewcontactService, ViewCompanyService) {
-
+        $scope.showLoader = true;
+        $scope.showUpdateBtn = false;
 	    var contactId = $stateParams.id;
 	    $scope.contactDetail = {};
         var value = "";
         ViewcontactService.getContact(contactId).then(function(result) {
           $scope.contactDetail = result;
+          $scope.showLoader = false;
         })
 
         $scope.contactIdList = {};
@@ -20,22 +22,26 @@
         ViewcontactService.getContactsList(contactId).then(function(list){
         	$scope.contactIdList = list;
 			index = $scope.contactIdList.indexOf(contactId);
-			selectedId = $scope.contactIdList[index]
+			selectedId = $scope.contactIdList[index];
         })
         
         $scope.nextContact = function(){
+            $scope.showLoader = true;
         	index = index + one;
         	selectedId = $scope.contactIdList[index];
 		        ViewcontactService.getContact(selectedId).then(function(result) {
 			    $scope.contactDetail = result;
+                $scope.showLoader = false;
 			})
         }
 
         $scope.prevContact = function(){
+            $scope.showLoader = true;
         	index = index - one;
         	selectedId = $scope.contactIdList[index];
 		        ViewcontactService.getContact(selectedId).then(function(result) {
 			       $scope.contactDetail = result;
+                   $scope.showLoader = false;
 			})
         }
 
@@ -74,27 +80,34 @@
 
         $scope.nameEdit = function(){
         	$scope.editName = false;
+            $scope.showUpdateBtn = true;
         }
         $scope.addressEdit = function(){
         	$scope.editAddress = false;
+            $scope.showUpdateBtn = true;
         }
         $scope.phoneEdit = function(){
         	$scope.editPhone = false;
+            $scope.showUpdateBtn = true;
         }
         $scope.mobileEdit = function(){
         	$scope.editMobile = false;
+            $scope.showUpdateBtn = true;
         }
         $scope.emailEdit = function(){
         	$scope.editEmail = false;
+            $scope.showUpdateBtn = true;
         }
         $scope.notesEdit = function(){
         	$scope.editContactNotes = false;
+            $scope.showUpdateBtn = true;
         }
 
         $scope.conData = {};
         $scope.contactData = {};
         $scope.contactData.contactList = [];
         $scope.updateContact = function(data){
+            $scope.showLoader = true;
         	$scope.editName = true;
 	        $scope.editCompany = true;
 	        $scope.editAddress = true;
@@ -125,11 +138,13 @@
 	            	toastr.success(''+result.success+'', {
 	            		closeButton: true
 	            	})
+                    $scope.showUpdateBtn = false;
 	            }else{
 	            	toastr.error(''+result.statusText+'', {
 	                	closeButton: true
 	                })
 	            }
+                $scope.showLoader = false;
 	        })
         }
 
