bb639860c4afc574f05cabbc136e08d944ee42ad
Rishav <rsingla.rishu@gmail.com>
2017-05-31 11:16:47 +0530

    account setting page implementation with api

--- app/partials/accountSetting/accountSetting.Controller.js
+++ app/partials/accountSetting/accountSetting.Controller.js
@@ -6,10 +6,57 @@
         .controller('AccountSettingController', [ '$scope', '$filter', '$rootScope', '$state', 'AccountSettingService', AccountSettingController]);
         
       function AccountSettingController($scope, $filter, $rootScope, $state, AccountSettingService) {
-          
+          $scope.addArray = [];
+          $scope.userData = {};
+          $scope.userid = JSON.parse(localStorage.getItem('userProfileId'));
+          if($scope.userid && $scope.userid != null && $scope.userid != undefined){
+          	var id = $scope.userid;
+          	AccountSettingService.loginUserData(id).then(function(result) {
+              	$scope.userData = result;
+              	$scope.userData.status = $scope.userData.account.user.status.toLowerCase();
+    		  		  $scope.userData.userType = $scope.userData.userType.type.toLowerCase();
+  	      	})
+          }
+
           AccountSettingService.getProducts().then(function(result) {
-                
+                $scope.productList = result;
+                for(var i=0;i<$scope.productList.length;i++){
+                  var obj = {
+                    id : $scope.productList[i].id,
+                    name : $scope.productList[i].name,
+                    status : $scope.productList[i].status
+                  }
+                  $scope.addArray.push(obj);
+                }
           })
+
+          $scope.productData = {};
+          $scope.productData.productList = [];
+          
+          $scope.checkProduct = function(productlist, product, index){
+              $scope.addArray[index].id =  productlist[index].id;
+              $scope.addArray[index].name =  productlist[index].name;
+              $scope.addArray[index].status =  productlist[index].status;
+              $scope.productData.productList = $scope.addArray;
+              console.log($scope.productData)
+          } 
+          
+          $scope.updateUser = function(){
+      		
+          	var updateData = "companyName=" + $scope.userData.companyName + "&username=" + $scope.userData.username + "&firstName=" + $scope.userData.firstName 
+          	+ "&lastName=" + $scope.userData.lastName + "&phone=" + $scope.userData.phone + "&mobile=" + $scope.userData.mobile 
+          	+ "&status=" + $scope.userData.status + "&userType=" + $scope.userData.userType + "&userTypeId=" + $scope.userData.userType.id + "&userProfileId=" + $scope.userid;
+
+            AccountSettingService.updateUserProfile(updateData).then(function(result) {
+
+              AccountSettingService.updateProducts($scope.productData).then(function(result) {
+                toastr.success(''+result.success+'', {
+                    closeButton: true
+                })
+              })
+	            
+            })
+          }
       }
 })();
 
--- app/partials/accountSetting/accountSetting.Service.js
+++ app/partials/accountSetting/accountSetting.Service.js
@@ -5,13 +5,66 @@
 
       function AccountSettingService($rootScope, $q, $http, $state, BASE_URL) {        
 
-        this.getProducts = function(data) {
+        this.getProducts = function() {
 
           var deferred = $q.defer();
           $http({
               method : 'GET',
               url : BASE_URL.url +'/user/products',
               headers : {'Content-Type': 'application/json'},
+          })
+          .then(function (result){
+              deferred.resolve(result.data);
+          },function (result){
+            //console.log(result.data)
+              deferred.resolve(result.data);
+          })
+          return deferred.promise;
+        }
+
+        this.updateProducts = function(data) {
+
+          var deferred = $q.defer();
+          $http({
+              method : 'PUT',
+              url : BASE_URL.url +'/fuelPricing/product',
+              data : data,
+              headers : {'Content-Type': 'application/json'},
+          })
+          .then(function (result){
+              deferred.resolve(result.data);
+          },function (result){
+            //console.log(result.data)
+              deferred.resolve(result.data);
+          })
+          return deferred.promise;
+        }
+
+        this.loginUserData = function(id) {
+
+          var deferred = $q.defer();
+          $http({
+              method : 'GET',
+              url : BASE_URL.url +'/account/user/'+id,
+              headers : {'Content-Type': 'application/json'},
+          })
+          .then(function (result){
+              deferred.resolve(result.data);
+          },function (result){
+            //console.log(result.data)
+              deferred.resolve(result.data);
+          })
+          return deferred.promise;
+        }
+
+        this.updateUserProfile = function(data) {
+
+          var deferred = $q.defer();
+          $http({
+              method : 'PUT',
+              url : BASE_URL.url +'/account/updateProfile',
+              headers : {'Content-Type': 'application/x-www-form-urlencoded'},
+              data: data
           })
           .then(function (result){
               deferred.resolve(result.data);
