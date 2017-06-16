64e7e5f508e58e86f3ff106148d8d5c61fd1acc2
Mr. Hot Foods <contact@mrhot.in>
2017-06-16 18:34:00 +0530

    view upload delete attachment fuel order

--- app/partials/fuelOrders/fuelOrders.service.js
+++ app/partials/fuelOrders/fuelOrders.service.js
@@ -6,27 +6,83 @@
   function fuelOrdersService($q, $http, BASE_URL) {        
 
     this.getAllCompanies = function() {
-      var deferred = $q.defer();
-      $http({
-        method : 'GET',
-        url : BASE_URL.url +'/user/allCompanies',
-        headers : {'Content-Type': 'application/json'},
-      })
-      .then(function (result){
-        deferred.resolve(result.data);
-      },function (result){
-        deferred.resolve(result.data);
-      })
-      return deferred.promise;
-    }
+                      var deferred = $q.defer();
+                      $http({
+                        method : 'GET',
+                        url : BASE_URL.url +'/user/allCompanies',
+                        headers : {'Content-Type': 'application/json'},
+                      })
+                      .then(function (result){
+                        deferred.resolve(result.data);
+                      },function (result){
+                        deferred.resolve(result.data);
+                      })
+                      return deferred.promise;
+                    }
 
     
       this.getOrders = function() {
 
+                      var deferred = $q.defer();
+                      $http({
+                          method : 'GET',
+                          url : BASE_URL.url +'/user/orders',
+                          headers : {'Content-Type': 'application/json'},
+                      })
+                      .then(function (result){
+                          deferred.resolve(result.data);
+                      },function (result){
+                          deferred.resolve(result.data);
+                      })
+                      return deferred.promise;
+                    }
+
+        
+
+    this.dispathFuelOrder = function(data) {
+                      var deferred = $q.defer();
+                      $http({
+                          method : 'POST',
+                          url : BASE_URL.url +'/fuelOrder',
+                          headers : {'Content-Type': 'application/json'},
+                          data: data
+                      })
+                      .then(function (result){
+                          deferred.resolve(result.data);
+                      },function (result){
+                          deferred.resolve(result.data);
+                      })
+                      return deferred.promise;
+                    }
+
+    this.uploadAttachment = function (data) {
+                    var fd = new FormData();
+                    
+                    angular.forEach(data, function(value, key) {
+                      fd.append(key, value);
+                    })
+                    
+                    var deffered = $q.defer();
+                    $http({
+                      method : 'POST',
+                      transformRequest: angular.identity,
+                      headers: {'Content-Type': undefined},
+                      url : BASE_URL.url+'/fuelOrder/upload/invoice',
+                      data : fd
+                    })
+                    .then(function (result){
+                      deffered.resolve(result);
+                    },function (result){
+                      deffered.resolve(result);
+                  })
+                    return deffered.promise;
+                  }
+
+        this.deleteAttachment = function(id){
           var deferred = $q.defer();
           $http({
-              method : 'GET',
-              url : BASE_URL.url +'/user/orders',
+              method : 'DELETE',
+              url : BASE_URL.url +'/fuelOrder/invoice/'+id,
               headers : {'Content-Type': 'application/json'},
           })
           .then(function (result){
@@ -36,73 +92,6 @@
           })
           return deferred.promise;
         }
-
-        
-
-    this.dispathFuelOrder = function(data) {
-      var deferred = $q.defer();
-      $http({
-          method : 'POST',
-          url : BASE_URL.url +'/fuelOrder',
-          headers : {'Content-Type': 'application/json'},
-          data: data
-      })
-      .then(function (result){
-          deferred.resolve(result.data);
-      },function (result){
-          deferred.resolve(result.data);
-      })
-      return deferred.promise;
-    }
-
-    this.uploadAttachment = function (data) {
-       var fd = new FormData();
-       
-       angular.forEach(data, function(value, key) {
-         fd.append(key, value);
-       })
-       
-       var deffered = $q.defer();
-       $http({
-         method : 'POST',
-         transformRequest: angular.identity,
-         headers: {'Content-Type': undefined},
-         url : BASE_URL.url+'/fuelOrder/upload/invoice',
-         data : fd
-       })
-       .then(function (result){
-	   		deffered.resolve(result);
-   		},function (result){
-   			deffered.resolve(result);
-		})
-       return deffered.promise;
-     }
-
-         this.deleteAttachment = function (data) {
-           var fd = new FormData();
-           
-           angular.forEach(data, function(value, key) {
-             fd.append(key, value);
-           })
-
-           var deffered = $q.defer();
-           $http({
-             method : 'POST',
-             transformRequest: angular.identity,
-             headers: {'Content-Type': undefined},
-             url : BE+'/fuelOrder/upload/invoice',
-             data : fd
-           })
-           .success(function (data, status, headers, config) {
-             //console.log(data);
-             deffered.resolve(status);
-           })
-           .error(function (data, status, headers, config) {
-             //console.log(data);
-             deffered.reject(status);
-           });
-           return deffered.promise;
-         }
 
 
 
