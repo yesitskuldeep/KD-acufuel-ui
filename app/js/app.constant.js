80cd89afbadfffee94c7d7641a2dc872991a8e64
Mr. Hot Foods <contact@mrhot.in>
2017-06-28 12:30:02 +0530

    vector maps

--- app/js/app.constant.js
+++ app/js/app.constant.js
@@ -4,9 +4,10 @@
 	angular.module('acufuel')
 	
 	.constant('BASE_URL', {
-		url: 'http://54.149.169.33:8080/ws'
+		// url: 'http://54.149.169.33:8080/ws'
         //url: 'http://192.168.1.175:8080/ws'
         //url: 'http://192.168.1.8:8080/ws'
+        url: 'http://192.168.2.137:8080/ws'
 
 	})
 
